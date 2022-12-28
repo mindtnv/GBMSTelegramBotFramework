fab6f3b1120a175a83cf4cebfbc04f223f8ad87a
mindtnv <47498401+mindtnv@users.noreply.github.com>
2022-12-28 19:56:19 +0300

    Add GBMSTelegramBotFramework.Testing

---
+++ src/GBMSTelegramBotFramework.Testing/UpdateBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Telegram.Bot.Types;
+
+namespace GBMSTelegramBotFramework.Testing;
+
+public class UpdateBuilder
+{
+    private readonly Update _update = new();
+
+    public UpdateBuilder WithId(int id)
+    {
+        _update.Id = id;
+        return this;
+    }
+
+    public UpdateBuilder WithMessage(Action<MessageBuilder> configure)
+    {
+        var builder = new MessageBuilder();
+        configure(builder);
+        _update.Message = builder.Build();
+        return this;
+    }
+
+    public Update Build() => _update;
+}
